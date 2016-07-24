350830798761d5f54146a43d3d280ca4e909b718
Nazmuz Shakib Pranto <npranto@gmail.com>
2016-07-23 22:22:58 -0500

    connecting front endpoint

--- public/controllers/dashboardCtrl.js
+++ public/controllers/dashboardCtrl.js
@@ -1,16 +1,17 @@
 angular.module('recruiterBot')
-	.controller('dashboardCtrl', function($scope, homeService){
+	.controller('dashboardCtrl', function($scope, dashboardService){
 		
+		// recruiterBot global variables
+		$scope.states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+		$scope.skills = ["HTML5", "CSS3", "Javascript", "Git", "GitHub", "jQuery", "AngularJS", "Node.JS", "Express", "Module Management", "React", "Webpack", "Flux", "Browserify", "Gulp", "MongoDB", "Firebase", "Bootstrap"];
+		$scope.selectedSkills = ["Bangladesh", "India", "Pakistan"];
+
+		// initial setup
 		$scope.showDashboard = true;
 		$scope.showStudent = false;
 		$scope.showAdmin = false;
 
-		$scope.states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-		$scope.skills = ["HTML5", "CSS3", "Javascript", "Git", "GitHub", "jQuery", "AngularJS", "Node.JS", "Express", "Module Management", "React", "Webpack", "Flux", "Browserify", "Gulp", "MongoDB", "Firebase", "Bootstrap"];
-
-		$scope.selectedSkills = ["Bangladesh", "India", "Pakistan"];
-
-
+		// header-tabs
 		$scope.showDashboardView = ()=>{
 			$scope.showDashboard = true;
 			$scope.showStudent = false;
@@ -28,16 +29,11 @@ angular.module('recruiterBot')
 			$scope.showAdmin = true;
 		}
 
-		// $scope.whenSkillAdded = (selectedSkill)=>{
-		// 	console.log(selectedSkill, selectedSkill.length);
-		// 	if (!selectedSkill.length) {
-		// 		$scope.showSkillAdd = false;
-		// 	}
-		// 	$scope.showSkillAdd = true;
-		// } 
+		// dashboard-jumbotron
 
+
+		// student-jumbotron
 		$scope.addToSkillsBox = (selectedSkill)=>{
-			
 			let selectedSkillToArray = selectedSkill.split(" ");
 			let skillFormatted = "" + selectedSkillToArray[0].charAt(0).toUpperCase() + selectedSkillToArray[0].slice(1);
 			for (var i = 1; i < selectedSkillToArray.length; i++) {
@@ -48,13 +44,22 @@ angular.module('recruiterBot')
 			}
 			$scope.selectedSkill = "";
 		}
-
 		$scope.removeSkill = (skill)=>{
 			let skillIndex = $scope.selectedSkills.indexOf(skill);
 			$scope.selectedSkills.splice(skillIndex, 1);
 		}
+		$scope.createStudent = (newStudent)=>{
+			
+		}
 
-
+		// admin-jumbotron
+		$scope.addAdmin = (newAdmin)=>{
+			console.log(newAdmin);
+			dashboardService.addAdmin(newAdmin)
+			// .then((response)=>{
+			// 	console.log(response);
+			// })
+		}
 
 
 
