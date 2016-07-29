7aa1702e59154b609d3a0bc604bd735fb62fc2e1
Nicodemus Swayne <nicodemus.swayne@gmail.com>
2016-07-29 11:02:48 -0500

    merge

--- server/sophiebot/config.js
+++ server/sophiebot/config.js
@@ -1,10 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
 
-  botToken: "",
-=======
-  botToken: "xoxbghvghvg787nbhbhjvjjj8upFNbkBQqi3oF",
->>>>>>> 38b134dfcf7634327911164fe96345e380f26653
+  botToken: "xoxb-62409945447-nTFgKAbiONIhpFNbkBQqi3oF",
   apiToken: "fd7fd2fe6f7e4250a5def953d97e4d92",
   apiClientToken: "01e26ce609af43fca247116f68712dad"
 }
