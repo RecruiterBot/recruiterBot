d98746da82190f5b79a52225c86c9473200238e9
Nicodemus Swayne <nicodemus.swayne@gmail.com>
2016-08-04 12:54:03 -0500

    limit search results to 10 or less

--- server/ctrls/sophieCtrl.js
+++ server/ctrls/sophieCtrl.js
@@ -1,5 +1,3 @@
-const Students = require( '../schemas/Students' );
-const attachmentCtrl = require( './attachmentCtrl' );
 const searchCtrl = require( './searchCtrl' );
 const deleteCtrl = require( './deleteCtrl' );
 const createCtrl = require( './createCtrl' );
@@ -7,7 +5,6 @@ const helpCtrl = require( './helpCtrl' );
 
 module.exports = ( ApiaiBotkit, Botkit, app, mongoURI ) => {
 
-  // const apiToken = require( '../sophiebot/config' ).apiToken;
   const controller = Botkit.slackbot();
 
   const bot = controller.spawn( {
@@ -15,11 +12,20 @@ module.exports = ( ApiaiBotkit, Botkit, app, mongoURI ) => {
   } );
 
   searchCtrl( bot, controller );
-  deleteCtrl( bot, controller );
-  createCtrl( bot, controller );
-  searchCtrl( bot, controller );
+  // deleteCtrl( bot, controller );
+  // createCtrl( bot, controller );
   helpCtrl( bot, controller );
 
+  bot.startRTM( ( err, bot, payload ) => {
+    if ( err ) {
+      console.log( err )
+      throw new Error( 'Could not connect to Slack' );
+    }
+
+  } );
+
+  
+  // const apiToken = require( '../sophiebot/config' ).apiToken;
   // const apiai = ApiaiBotkit( apiToken );
 
   // controller.hears('.*', 'direct_message,direct_mention,mention', ( bot, message ) => {
@@ -64,13 +70,6 @@ module.exports = ( ApiaiBotkit, Botkit, app, mongoURI ) => {
   //   } )
   // } )
 
-  bot.startRTM( ( err, bot, payload ) => {
-    if ( err ) {
-      console.log( err )
-      throw new Error( 'Could not connect to Slack' );
-    }
-
-  } );
 }
 
 
